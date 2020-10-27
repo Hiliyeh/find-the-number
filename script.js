1657bed1bb14531cc6f165a983087f78d5a182db
Hiliyeh Bashir <hiliyeh.bashir@gmail.com>
2020-10-27 12:36:14 +0100

    logic debut

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 'use strict'
+
+//secret number
+const secretNumber = Math.trunc(Math.random()*20)+1;
+//score
+let score=20;
+
+
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click',function(){
+
+  const guess= Number(document.querySelector('.guess').value) ; 
+
+    if(!guess){
+       document.querySelector('.message').textContent="we need a number";
+    }else if(guess > secretNumber ){
+
+        if(score > 1){ document.querySelector('.message').textContent="to high";
+    document.querySelector('.score').textContent=score;
+    score--;} else{
+               document.querySelector('.message').textContent="you lost the game";
+
+    }
+   
+   
+    }else if(guess < secretNumber){
+    document.querySelector('.message').textContent="to low";
+      document.querySelector('.score').textContent=score;
+    score--;
+   
+}
+})
